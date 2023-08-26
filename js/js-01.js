1aca70b6e13cc1dc1b88910be145e124e78e03e5
umer2581 <mohmmedumer866@gmail.com>
2023-08-26 09:48:14 +0530

    Js start

---
+++ js/js-01.js
@@ -0,0 +1,6 @@
+// console.log is used to print something in developer tool 
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("print something");
