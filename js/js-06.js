9ef3dcfe2295cf1703bf553c24c4c21531ae24b2
umer2581 <mohmmedumer866@gmail.com>
2023-09-05 09:39:07 +0530

    Updated files

---
+++ js/js-06.js
@@ -0,0 +1,29 @@
+//  INDEXING OF A STRING 
+// in indexing we always start with from 0 value
+
+let str="MO UMER"
+console.log(str);
+
+// access characters with index value
+console.log(str[0]);
+console.log(str[1]);
+console.log(str[2]);
+console.log(str[3]);
+console.log(str[4]);
+console.log(str[5]);
+console.log(str[6]);
+
+
+//to find string of a length we use .LENGTH with variable
+
+let str1="mo umer"
+console.log(str1.length);
+
+//to find last character of value we use .LENGTH-1 
+
+let str2="dfjkldfkldlkjfasljdflasjr";
+console.log(str2[str2.length-1]);
+//to find any character of value we use .LENGTH-n of numbers
+
+let str3="dfjkldfkldlkjfasljdflasjr";
+console.log(str3[str3.length-4]);
---
+++ js/js-07.js
@@ -0,0 +1,38 @@
+// string methods
+// trim
+// to uppercase
+// to lowercase
+//slice
+let firstname="  rajesh    ";
+console.log(firstname);
+console.log(firstname.length);
+console.log(firstname[firstname.length-5]);
+
+// TRIM METHOD
+// this method will reduce all spaces from your string
+firstname=firstname.trim();
+console.log(firstname); // rajesh 
+console.log(firstname.length); // after trim this will give 6
+
+// TO upperCase METHOD
+
+let value="umer naaz"
+console.log(value);
+value=value.toUpperCase();
+console.log(value);
+
+// TO lowerCase METHOD
+
+let value1="UMER NAAZ";
+console.log(value);
+value1=value.toLowerCase();
+console.log(value1);
+
+
+// SLICE METHOD (this value will execute with one skip value)
+
+let value2="javednaaz";
+value2=value2.slice(1,7); // it will only avedn
+console.log(value2);
+
+
