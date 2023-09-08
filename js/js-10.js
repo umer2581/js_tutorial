5f8355823608e5fcc134b0842621cadfe61d7066
umer2581 <mohmmedumer866@gmail.com>
2023-09-08 09:57:25 +0530

    updated

--- js/js-10.js
+++ js/js-10.js
@@ -1,1 +1,27 @@
 // Template string
+ 
+// simple concatenation
+let str1="abdul";
+let str2="salam";
+let fname=str1 + " " + str2;
+console.log(fname);
+console.log(typeof fname);
+
+// in template form
+ let str3="my name is ";
+ let name="umer";
+ let str4="and my age is ";
+ let age=20;
+ 
+ let tot= `${str3} ${name} ${str4} ${age}` ; // my name is umer and my age is 20
+ console.log(tot);
+
+ let st0="umer said to salam ";
+ let qu="ASSALAMUALEKUM";
+ let st1="in return back salam said";
+ let ans="WALEKUMASSALAM";
+
+ let merg=`${st0} ${qu} ${st1} ${ans}`; // umer said to salam ASSALAMUALEKUM in return back salam said WALEKUMASSALAM
+
+ console.log(merg);
+ console.log(typeof merg);
---
+++ js/js-11.js
@@ -0,0 +1,24 @@
+// undefined
+ let umer;
+ console.log(umer);
+ var umer1;
+ console.log(umer1);
+//  const umer2;
+//  console.log(umer2); this will be give error 
+
+// null
+ let khan=null;
+ console.log(typeof khan); // this will give object and its a bug in js which will be not correct 
+ console.log(khan);
+
+ // BIGINT
+ // in bigint we have limited space to store our variable and to store big values we use bigint keyword
+ // with bigint we cannnot use any other data type this will gives us error
+
+ let number=BigInt(121213123);
+ console.log(number);
+
+ let num2=1234n;
+ console.log(num2);
+ // we have two methods to use bigint first one is use bigint function with value and the second is use n with the end of number
+ 
